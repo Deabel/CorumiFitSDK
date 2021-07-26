a13327f0f101cde15bf9f8fd2d47f0ee563f13db
CorumiFit <deabel@163.com>
2021-07-26 12:28:57 +0800

    添加sdk

---
+++ CFDeviceKit.xcframework/ios-x86_64-simulator/CFDeviceKit.framework/Headers/CFDeviceKit.h
@@ -0,0 +1,19 @@
+//
+//  CFDeviceKit.h
+//  CFDeviceKit
+//
+//  Created by Deabel on 2021/6/28.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+//! Project version number for CFDeviceKit.
+FOUNDATION_EXPORT double CFDeviceKitVersionNumber;
+
+//! Project version string for CFDeviceKit.
+FOUNDATION_EXPORT const unsigned char CFDeviceKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CFDeviceKit/PublicHeader.h>
+
+
